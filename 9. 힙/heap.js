3b61d9f4ded814d300b29197f40319f4c0c93404
zzmin13 <zzzz1997@naver.com>
2021-04-29 11:06:39 +0900

    힙 수정

--- 9. 힙/heap.js
+++ 9. 힙/heap.js
@@ -1,126 +1,140 @@
-class Heap{
-    constructor(){
-        this.items = [];
-    }
+class Heap {
+  constructor() {
+    this.items = [];
+  }
 
-    //값을 서로 바꾸는 메소드
-    swap(index1, index2){
-        let temp = this.items[index1]; // items의 index1의 값을 temp(임시공간)에 담음
-        this.items[index1] = this.items[index2]; // index1에 index2의 값을 저장
-        this.items[index2] = temp; // index2에 아까 index1의 값을 temp에 넣어놓은 값을 저장
-    }
+  //값을 서로 바꾸는 메소드
+  swap(index1, index2) {
+    let temp = this.items[index1]; // items의 index1의 값을 temp(임시공간)에 담음
+    this.items[index1] = this.items[index2]; // index1에 index2의 값을 저장
+    this.items[index2] = temp; // index2에 아까 index1의 값을 temp에 넣어놓은 값을 저장
+  }
 
-    //부모 인덱스 구하는 메소드
-    parentIndex(index){
-        return Math.floor((index-1) / 2);
-    }
+  //부모 인덱스 구하는 메소드
+  parentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
 
-    //왼쪽 자식 인덱스 구하는 메소드
-    leftChildIndex(index){
-        return index * 2 + 1;
-    }
+  //왼쪽 자식 인덱스 구하는 메소드
+  leftChildIndex(index) {
+    return index * 2 + 1;
+  }
 
-    //오른쪽 자식 인덱스 구하는 메소드
-    rightChildIndex(index){
-        return index * 2 + 2;
-    }
+  //오른쪽 자식 인덱스 구하는 메소드
+  rightChildIndex(index) {
+    return index * 2 + 2;
+  }
 
-    //부모 노드 구하는 메소드
-    parent(index){
-        return this.items[this.parentIndex(index)];
-    }
+  //부모 노드 구하는 메소드
+  parent(index) {
+    return this.items[this.parentIndex(index)];
+  }
 
-    //왼쪽 자식 노드 구하는 메소드
-    leftChild(index){
-        return this.items[this.leftChildIndex(index)];
-    }
+  //왼쪽 자식 노드 구하는 메소드
+  leftChild(index) {
+    return this.items[this.leftChildIndex(index)];
+  }
 
-    //오른쪽 자식 노드 구하는 메소드
-    rightChild(index){
-        return this.items[this.rightChildIndex(index)];
-    }
+  //오른쪽 자식 노드 구하는 메소드
+  rightChild(index) {
+    return this.items[this.rightChildIndex(index)];
+  }
 
-    //최대 힙의 경우 최댓값을 반환하고 최소 힙의 경우 최솟값을 반환하는 메소드
-    peek(){
-        return this.items[0];
-    }
+  //최대 힙의 경우 최댓값을 반환하고 최소 힙의 경우 최솟값을 반환하는 메소드
+  peek() {
+    return this.items[0];
+  }
 
-    //힙의 크기(항목 개수)를 반환하는 메소드
-    size(){
-        return this.items.length;
-    }
+  //힙의 크기(항목 개수)를 반환하는 메소드
+  size() {
+    return this.items.length;
+  }
 }
 
-class MinHeap extends Heap{
-    // MinHeap 클래스는 Heap 클래스를 상속받았으므로 Heap 클래스의 메소드를 모두 사용할 수 있다.
+class MinHeap extends Heap {
+  // MinHeap 클래스는 Heap 클래스를 상속받았으므로 Heap 클래스의 메소드를 모두 사용할 수 있다.
 
-    bubbleUp(){
-        let index = this.items.length-1;
-        while(this.parent(index) && this.parent(index) > this.items[index]){
-            this.swap(index, this.parentIndex(index));
-            index = this.parentIndex(index);
-        }
+  bubbleUp() {
+    let index = this.items.length - 1;
+    while (this.parent(index) && this.parent(index) > this.items[index]) {
+      this.swap(index, this.parentIndex(index));
+      index = this.parentIndex(index);
     }
+  }
 
-    bubbleDown(){
-        let index = 0;
+  bubbleDown() {
+    let index = 0;
 
-        while(this.leftChild(index) && this.leftChild(index) < this.items[index]){
-            let smallerIndex = this.leftChildIndex(index);
-            if(this.rightChild(index) && this.rightChild(index) < this.items[smallerIndex]){
-                smallerIndex = this.rightChildIndex(index);
-            }
-            this.swap(index, smallerIndex);
-            index = smallerIndex;
-        }
+    while (
+      this.leftChild(index) &&
+      (this.leftChild(index) < this.items[index] ||
+        this.rightChild(index) < this.items[index])
+    ) {
+      let smallerIndex = this.leftChildIndex(index);
+      if (
+        this.rightChild(index) &&
+        this.rightChild(index) < this.items[smallerIndex]
+      ) {
+        smallerIndex = this.rightChildIndex(index);
+      }
+      this.swap(index, smallerIndex);
+      index = smallerIndex;
     }
+  }
 
-    // 힙에 원소를 추가하는 함수
-    add(item){
-        this.items[this.items.length] = item;
-        this.bubbleUp();
-    }
+  // 힙에 원소를 추가하는 함수
+  add(item) {
+    this.items[this.items.length] = item;
+    this.bubbleUp();
+  }
 
-    // 힙에서 원소를 빼내는 함수
-    // 최소 힙이라면 최솟값이 빠져나올 것이고 최대힙이라면 최댓값이 빠져나온다.
-    poll(){
-        let item = this.items[0]; // 첫번째 원소 keep
-        this.items[0] = this.items[this.items.length - 1]; // 맨 마지막 원소를 첫번째 원소로 복사
-        this.items.pop(); // 맨 마지막 원소 삭제
-        this.bubbleDown();
+  // 힙에서 원소를 빼내는 함수
+  // 최소 힙이라면 최솟값이 빠져나올 것이고 최대힙이라면 최댓값이 빠져나온다.
+  poll() {
+    let item = this.items[0]; // 첫번째 원소 keep
+    this.items[0] = this.items[this.items.length - 1]; // 맨 마지막 원소를 첫번째 원소로 복사
+    this.items.pop(); // 맨 마지막 원소 삭제
+    this.bubbleDown();
 
-        return item; // keep해둔 값 반환
+    return item; // keep해둔 값 반환
+  }
+  sort() {
+    let sort = [];
+    const count = this.items.length;
+    for (let i = 0; i < count; i++) {
+      sort.push(this.poll());
     }
-    sort(){
-        let sort = [];
-        const count = this.items.length;
-        for(let i = 0; i < count; i++){
-            sort.push(this.poll());
-        }
-        return sort;
-    }
+    return sort;
+  }
 }
 
-class MaxHeap extends MinHeap{
-    //MaxHeap의 경우 MinHeap을 상속받았으므로 MinHeap의 모든 함수를 사용할 수 있지만 bubbleUp과 bubbleDown은 Overriding(재정의)하였다.
-    bubbleUp(){
-        let index = this.items.length - 1;
-        while(this.parent(index) && this.parent(index) < this.items[index]){
-            this.swap(index, this.parentIndex(index));
-            index = this.parentIndex(index);
-        }
+class MaxHeap extends MinHeap {
+  //MaxHeap의 경우 MinHeap을 상속받았으므로 MinHeap의 모든 함수를 사용할 수 있지만 bubbleUp과 bubbleDown은 Overriding(재정의)하였다.
+  bubbleUp() {
+    let index = this.items.length - 1;
+    while (this.parent(index) && this.parent(index) < this.items[index]) {
+      this.swap(index, this.parentIndex(index));
+      index = this.parentIndex(index);
     }
+  }
 
-    bubbleDown(){
-        let index = 0;
-        
-        while(this.leftChild(index) && this.leftChild(index) > this.items[index]){
-            let largerIndex = this.leftChildIndex(index);
-            if(this.rightChild(index) && this.rightChild(index) > this.items[largerIndex]){
-                largerIndex = this.rightChildIndex(index);
-            }
-            this.swap(largerIndex, index);
-            index = largerIndex;
-        }
+  bubbleDown() {
+    let index = 0;
+
+    while (
+      this.leftChild(index) &&
+      (this.leftChild(index) > this.items[index] ||
+        this.rightChild(index) > this.items[index])
+    ) {
+      let largerIndex = this.leftChildIndex(index);
+      if (
+        this.rightChild(index) &&
+        this.rightChild(index) > this.items[largerIndex]
+      ) {
+        largerIndex = this.rightChildIndex(index);
+      }
+      this.swap(largerIndex, index);
+      index = largerIndex;
     }
+  }
 }
